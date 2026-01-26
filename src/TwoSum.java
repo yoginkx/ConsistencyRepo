ad9f293e41e67882fc13aec95a7eded1071dbebb
Yogisgit <yogeshravi2704@gmail.com>
2026-01-26 15:08:22 +0530

    Two Sum Efficient

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -33,19 +33,39 @@ Only one valid answer exists.
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
  */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
-        int arr[] = new int[2];
+
+    public static void main(String[] args) {
+        int nums[] = {2,5,5,11,3,3};
+        int arr[] = twoSum(nums, 10);
+        //for (int a: arr) System.out.print(a);
+        int arr1[] = twoSumEff(nums, 6);
+        for (int a: arr1) System.out.print(a);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
         for(int i=0; i<nums.length; i++){
-            for( int j=1; j<nums.length-1; j++){
+            for( int j=i+1; j<nums.length; j++){
                 if((nums[i] + nums[j]) == target)
                 {
-                    arr[0] = i;
-                    arr[1] = j;
-                    return arr;
+                    return new int[] {i,j};
                 }
             }
         }
-        return arr;
+        return new int[]{0};
+    }
+
+    public static int[] twoSumEff(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)) return new int[] {map.get(complement), i};
+            map.put(nums[i], i);
+        }
+        return new int[]{};
     }
 }
